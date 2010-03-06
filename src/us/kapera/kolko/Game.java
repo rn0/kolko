7f6883e81e01cdcba08f7f501bfaa0fe15d68d3b
rn0 <rennoo@gmail.com>
2010-03-06 21:24:14 +0100

    initial commit

---
+++ src/us/kapera/kolko/Game.java
@@ -0,0 +1,12 @@
+package us.kapera.kolko;
+
+/**
+ * User: piotr
+ * Date: 2010-03-06
+ * Time: 21:13:38
+ */
+public class Game {
+  public static void main(String[] arg) {
+    System.out.print("Hello");
+  }
+}
