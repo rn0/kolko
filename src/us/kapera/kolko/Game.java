7f79a9e95f40723d60ee7830ddd4db75123ad5ae
rn0 <rennoo@gmail.com>
2010-03-06 23:34:47 +0100

    gui without idea builtin designer

--- src/us/kapera/kolko/Game.java
+++ src/us/kapera/kolko/Game.java
@@ -1,5 +1,7 @@
 package us.kapera.kolko;
 
+import us.kapera.kolko.gui.Board;
+
 /**
  * User: piotr
  * Date: 2010-03-06
@@ -8,5 +10,7 @@ package us.kapera.kolko;
 public class Game {
   public static void main(String[] arg) {
     System.out.print("Hello");
+
+    new Board();
   }
 }
---
+++ src/us/kapera/kolko/gui/Board.java
@@ -0,0 +1,26 @@
+package us.kapera.kolko.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * User: piotr
+ * Date: 2010-03-06
+ * Time: 23:08:35
+ */
+public class Board extends JFrame {
+  public Board() {
+    setTitle("Kółko i krzyżyk");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    Container pane = getContentPane();
+    pane.setLayout(new GridLayout(0,3));
+    JButton buttons[] = new JButton[20];
+    for(int i = 0; i <= 8; i++) {
+      pane.add(buttons[i] = new JButton("Button " + (i + 1)));
+    }
+    
+    setSize(400, 400);
+    setVisible(true);
+  }
+}
